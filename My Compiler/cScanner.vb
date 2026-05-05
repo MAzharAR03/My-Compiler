8f008b572c014a08b7d0fe764b167d393ad8e705
MAzharAR03 <1221100800@student.mmu.edu.my>
2026-05-05 21:38:01 +0800

    Tutorial 5 basically done

--- My Compiler/cScanner.vb
+++ My Compiler/cScanner.vb
@@ -27,29 +27,63 @@ Public Class cScanner
         If currentIndex = frmMyCompiler.txtProgram.Text.Length Then
             Return cToken.EOF
         End If
-        If Char.IsLetter(currentChar) Then
+        'If Char.IsLetter(currentChar) Then
+        '    takeIt()
+        '    state = 2
+        '    While Char.IsLetterOrDigit(currentChar)
+        '        takeIt()
+        '    End While
+        'ElseIf Char.IsDigit(currentChar) Then
+        '    takeIt()
+        '    state = 3
+        '    While Char.IsDigit(currentChar)
+        '        takeIt()
+        '    End While
+        'ElseIf "+-*/=".Contains(currentChar) Then
+        '    takeIt()
+        '    state = 4
+        'End If
+        If currentChar = "A" Or currentChar = "B" Or currentChar = "C" Then
             takeIt()
             state = 2
-            While Char.IsLetterOrDigit(currentChar)
-                takeIt()
-            End While
-        ElseIf Char.IsDigit(currentChar) Then
+        ElseIf currentChar = "=" Then
             takeIt()
             state = 3
-            While Char.IsDigit(currentChar)
-                takeIt()
-            End While
-        ElseIf "+-*/=".Contains(currentChar) Then
+        ElseIf currentChar = ";" Then
             takeIt()
             state = 4
+        ElseIf currentChar = "+" Then
+            takeIt()
+            state = 5
+        ElseIf currentChar = "*" Then
+            takeIt()
+            state = 6
+        ElseIf currentChar = "(" Then
+            takeIt()
+            state = 7
+        ElseIf currentChar = ")" Then
+            takeIt()
+            state = 8
         End If
         Select Case state
             Case 2
                 Return cToken.IDENTIFIER
+                'Case 3
+                '    Return cToken.INTEGERTYPE
+                'Case 4
+                '    Return cToken.OPERATORTYPE
             Case 3
-                Return cToken.INTEGERTYPE
+                Return cToken.ASSIGNMENT
             Case 4
-                Return cToken.OPERATORTYPE
+                Return cToken.SEMICOLON
+            Case 5
+                Return cToken.Plus
+            Case 6
+                Return cToken.Multiply
+            Case 7
+                Return cToken.LeftPara
+            Case 8
+                Return cToken.RightPara
             Case Else
                 Return cToken.UNKNOWN
         End Select
--- My Compiler/cToken.vb
+++ My Compiler/cToken.vb
@@ -13,17 +13,35 @@
             Return "EOF: " & spelling
         ElseIf kind = 2 Then
             Return "IDENTIFIER: " & spelling
+            'ElseIf kind = 3 Then
+            '    Return "INTEGERTYPE: " & spelling
+            'Else
+            '    Return "OPERATORTYPE: " & spelling
         ElseIf kind = 3 Then
-            Return "INTEGERTYPE: " & spelling
-        Else
-            Return "OPERATORTYPE: " & spelling
+            Return "ASSIGNMENT: " & spelling
+        ElseIf kind = 4 Then
+            Return "SEMICOLON: " & spelling
+        ElseIf kind = 5 Then
+            Return "Plus: " & spelling
+        ElseIf kind = 6 Then
+            Return "Multiply: " & spelling
+        ElseIf kind = 7 Then
+            Return "LeftPara: " & spelling
+        ElseIf kind = 8 Then
+            Return "RightPara: " & spelling
         End If
     End Function
 
     Public Shared UNKNOWN As Integer = 0
     Public Shared EOF As Integer = 1
     Public Shared IDENTIFIER As Integer = 2
-    Public Shared INTEGERTYPE As Integer = 3
-    Public Shared OPERATORTYPE As Integer = 4
-    Public Shared MNEMONIC As Integer = 5
+    'Public Shared INTEGERTYPE As Integer = 3
+    'Public Shared OPERATORTYPE As Integer = 4
+    'Public Shared MNEMONIC As Integer = 5
+    Public Shared ASSIGNMENT As Integer = 3
+    Public Shared SEMICOLON As Integer = 4
+    Public Shared Plus As Integer = 5
+    Public Shared Multiply As Integer = 6
+    Public Shared LeftPara As Integer = 7
+    Public Shared RightPara As Integer = 8
 End Class
